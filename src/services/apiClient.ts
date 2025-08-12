8274ecc509a6e90de1d895bc08ca81f2e28f1a23
Babloo Kannojia <71953701+BablooKannojia@users.noreply.github.com>
2025-08-12 14:44:46 +0530

    Update apiClient.ts

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://v-commission.onrender.com';
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
